186a9091ef19f6a717f0f2aa0c0136497ffebb2b
Ian Fairman <ian.fairman@gmail.com>
2025-10-02 10:22:08 +0100

    Added licenses. Removed unused imports.

--- src/main/java/io/github/ianfairman/fasta/parser/FastaParser.java
+++ src/main/java/io/github/ianfairman/fasta/parser/FastaParser.java
@@ -1,11 +1,24 @@
+/*
+ * Copyright 2025 Ian Fairman.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.ianfairman.fasta.parser;
 
 import io.github.ianfairman.io.IORuntimeException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.io.StringReader;
-import java.util.ArrayList;
 import static java.util.Collections.emptyList;
 import java.util.List;
 import static java.util.Objects.requireNonNull;
--- src/main/java/io/github/ianfairman/io/IORuntimeException.java
+++ src/main/java/io/github/ianfairman/io/IORuntimeException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 hp.
+ * Copyright 2025 Ian Fairman.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
